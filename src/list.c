3e4823dc1a7e36dec1c97636191bcfb62a628fed
batmacumba <batmacumba@protonmail.com>
2018-04-27 19:58:15 -0300

    get and set ready

--- src/list.c
+++ src/list.c
@@ -12,59 +12,49 @@
 #undef list_append
 #undef list_prepend
 #undef list_insert
+#undef list_set
 
-/* Data Structures */
+/* Data Structure */
 struct List {
-    void** items;
-    int capacity;                       // allocated storage capacity
-    int size;                           // number of inserted elements
-    int zero;                           // offsets the beggining of the array
+    void **items;
+    int allocated;                      // allocated storage capacity
+    int n;                              // number of elements
+    int zero;                           // index of the first element
 };
 
-// testing
-void print_list(List *l) {
-    for (int i = 0; i < l -> capacity; i++) {
-        int *ptr = (int*) l -> items[i];
-        if (ptr != NULL)
-            printf("%d = %d\n", i, ptr);
-        else
-            printf("%d = NULL\n", i);
-    }
-}
 /**
  *  list_new():
  *      Initializes an empty list and returns a pointer to it.
  */
-List *list_new()
-{
-    List *l = (List*) malloc(sizeof(List));
-    if (!l) {
+List *
+list_new() {
+    List *this = (List*) malloc(sizeof(List));
+    if (!this) {
         fprintf(stderr, "list_new: cannot create new list!\n");
         return NULL;
     }
-    l -> items = (void**) malloc(sizeof(void*));
-    l -> size = 0;
-    l -> capacity = 1;
-    l -> zero = 0;
-    return l;
+    this -> items = NULL;
+    this -> n = 0;
+    this -> allocated = 0;
+    this -> zero = 0;
+    return this;
 }
 
 /**
- *  list_upsize():
- *      Resizes the list to the given capacity argument.
- *      Returns 0 if the list was succesfully resized and 1 otherwise.
- *      If resizing is not successful, list remains unaltered.
+ *  list_resize():
+ *      Resizes the list to the given argument.
  */
-int list_upsize(List *l, int capacity, int prepend) {
+static int
+list_resize(List *l, int allocated, int prepend) {
     /* size of the new block of memory */
-    size_t memSize = capacity * sizeof(void*);
+    size_t memSize = allocated * sizeof(void*);
     void **i;
     /* prepend allocates memory and copies data back into the second half */
     if (prepend) {
         i = (void**) malloc(memSize);
-        memcpy(i + (capacity / 2) , l -> items, memSize / 2);
+        memcpy(i + (allocated / 2) , l -> items, memSize / 2);
         /* important! offsets the "zero element" on the list */
-        l -> zero = capacity / 2;
+        l -> zero = allocated / 2;
     }
     /* append simply tries to allocate more memory on the existing block */
     else {
@@ -72,98 +62,144 @@ int list_upsize(List *l, int capacity, int prepend) {
     }
     /* error checking and variable updating */
     if(!i) {
-        fprintf(stderr, "list_upsize: cannot resize list!\n");
-        return 1;
+        fprintf(stderr, "list_resize: cannot resize list!\n");
+        return -1;
     }
     l -> items = i;
-    l -> capacity = capacity;
+    l -> allocated = allocated;
     return 0;
 }
 
 /**
- *  list_append():
- *      Adds an item to the end of the list. Returns 0 if everything goes as
- *      expected and 1 otherwise.
+ *  list_memHandler():
+ *      Checks if the list needs to shrink or grow and acts accordingly.
  */
-int list_append(List *l, void *data, size_t dataSize) {
-    /* if the list is full, resize it to two times it's current capacity */
-    if (l -> size == l -> capacity) {
-        if (list_upsize(l, 2 * l -> capacity, 0)) {
-            fprintf(stderr, "list_append: cannot append to list!\n");
-            return 1;
+static int
+list_memHandler(List *self, int flag) {
+    /* if full, resizes it to 1.5x it's currently allocated capacity */
+    if (self -> n == self -> allocated) {
+        if (list_resize(self, 1.5 * self -> allocated, 0) < 0) {
+            fprintf(stderr, "list_memHandler: cannot append to list!\n");
+            return -1;
         }
     }
-    /* allocates space on the heap and copies the data */
-    l -> items[l -> size] = malloc(dataSize);
-    memcpy(l -> items[l -> size++], data, dataSize);
-    return 0;
 }
-
-/**
- *  list_prepend():
- *      Adds an item to the beggining of the list. Returns 0 if everything goes
- *      as expected and 1 otherwise.
- */
-int list_prepend(List *l, void *data, size_t dataSize) {
-    /* if the list is full, resize it to two times it's current capacity */
-    if (l -> size == l -> capacity || l -> zero == 0) {
-        if (list_upsize(l, 2 * l -> capacity, 1)) {
-            fprintf(stderr, "list_prepend: cannot prepend to list!\n");
-            return 1;
-        }
-    }
-    /* allocates space on the heap and copies the data */
-    if (l -> zero > 0) l -> zero--;
-    l -> items[l -> zero] = malloc(dataSize);
-    memcpy(l -> items[l -> zero], data, dataSize);
-    l -> size++;
-    return 0;
-}
-
-/**
- *  list_insert():
- *      Inserts an item at the given index. Returns 0 if everything goes as
- *      expected and 1 otherwise.
- */
-int list_insert(List *l, int i, void *data, size_t dataSize) {
-    if (i > l -> size - 1) {
-        fprintf(stderr, "list_insert: index out of range!\n");
-        return 1;
-    }
-    /* inserting at the last index is equivalent to appending */
-    else if (i == l -> size) return list_append(l, data, dataSize);
-    /* inserting at the zero index is equivalent to prepending */
-    else if (i == 0) return list_prepend(l, data, dataSize);
-    /* otherwise, replace an existing value */
-    else {
-        memcpy(l -> items[i], data, dataSize);
-        return 0;
-    }
-}
+///**
+// *  list_append():
+// *      Adds an item to the end of the list.
+// */
+//int
+//list_append(List *l, void *data, size_t dataSize) {
+//
+//    /* allocates space on the heap and copies the data */
+//    l -> items[l -> n] = malloc(dataSize);
+//    memcpy(l -> items[l -> n++], data, dataSize);
+//    return 0;
+//}
+//
+///**
+// *  list_prepend():
+// *      Adds an item to the beggining of the list. Returns 0 if everything goes
+// *      as expected and 1 otherwise.
+// */
+//int list_prepend(List *l, void *data, size_t dataSize) {
+//    /* if the list is full, resize it to two times it's current allocated */
+//    if (l -> n == l -> allocated || l -> zero == 0) {
+//        if (list_resize(l, 2 * l -> allocated, 1)) {
+//            fprintf(stderr, "list_prepend: cannot prepend to list!\n");
+//            return -1;
+//        }
+//    }
+//    /* allocates space on the heap and copies the data */
+//    if (l -> zero > 0) l -> zero--;
+//    l -> items[l -> zero] = malloc(dataSize);
+//    memcpy(l -> items[l -> zero], data, dataSize);
+//    l -> n++;
+//    return 0;
+//}
+//
+///**
+// *  list_insert():
+// *      Inserts an item at the given index. Returns 0 if everything goes as
+// *      expected and 1 otherwise.
+// */
+//int list_insert(List *l, int i, void *data, size_t dataSize) {
+//    if (i > l -> n - 1) {
+//        fprintf(stderr, "list_insert: index out of range!\n");
+//        return -1;
+//    }
+//    /* inserting at the last index is equivalent to appending */
+//    else if (i == l -> n) return list_append(l, data, dataSize);
+//    /* inserting at the zero index is equivalent to prepending */
+//    else if (i == 0) return list_prepend(l, data, dataSize);
+//    /* otherwise, replace an existing value */
+//    else {
+//        memcpy(l -> items[i], data, dataSize);
+//        return 0;
+//    }
+//}
 
 /**
  *  list_get():
- *      Copies the item at the given index and returns it.
+ *      Returns the item at the given index.
  */
-void *list_get(List *l, int i) {
-    if (i > l -> size - 1) {
-        fprintf(stderr, "list_get: index out of range!\n");
+void *
+list_get(List *op, int i) {
+    if (i > op -> n - 1) {
+        fprintf(stderr, "list_get: index out of range\n");
         return NULL;
     }
-    /* TODO: copy data before returning? */
-    return l -> items[l -> zero + i];
+    /* negative index returns n - i item */
+    if (i < 0) {
+        i += op -> n;
+        if (i < 0) {
+            fprintf(stderr, "list_get: index out of range\n");
+            return NULL;
+        }
+    }
+    return op -> items[op -> zero + i];
+}
+
+/**
+ *  list_set():
+ *      Sets the item at the given index to the given value.
+ */
+int
+list_set(List *op, int i, void *data, size_t dataSize) {
+    if (i > op -> n - 1) {
+        fprintf(stderr, "list_set: index out of range\n");
+        return -1;
+    }
+    /* negative index sets n - i item */
+    if (i < 0) {
+        i += op -> n;
+        if (i < 0) {
+            fprintf(stderr, "list_set: index out of range\n");
+            return -1;
+        }
+    }
+    
+    op -> items[op -> zero + i] = malloc(dataSize);
+    if (!op -> items[op -> zero + i]) {
+        fprintf(stderr, "list_set: cannot allocate space\n");
+        return -1;
+    }
+    /* finally copies the data and returns */
+    memcpy(op -> items[op -> zero + i], data, dataSize);
+    return 0;
 }
 
 #define list_append(l, data) list_append(l, data, sizeof(data))
 #define list_prepend(l, data) list_prepend(l, data, sizeof(data))
 #define list_insert(l, i, data) list_append(l, i, data, sizeof(data))
+#define list_set(l, i, data) list_set(l, i, data, sizeof(data))
 
 int main() {
     List *l = list_new();
     int n = 4;
-    for (int i = 0; i < n; i++)
-        list_prepend(l, &i);
-    print_list(l);
+//    for (int i = 0; i < n; i++)
+//        list_prepend(l, &i);
+//    print_list(l);
     return 0;
 }
 
--- src/list.h
+++ src/list.h
@@ -10,16 +10,16 @@
 #ifndef _LIST_H_
 #define _LIST_H_
 
-/* Struct */
+/* Data Structure */
 typedef struct List List;
 
 /* Operations */
 List*   list_new();
 void*   list_get(List *l, int i);
-int     list_put(List *l, int i, void *data, size_t dataSize);
-int     list_insert(List *l, int i, void *data, size_t dataSize);
+int     list_set(List *l, int i, void *data, size_t dataSize);
 int     list_append(List *l, void *data, size_t dataSize);
 int     list_prepend(List *l, void *data, size_t dataSize);
+int     list_insert(List *l, int i, void *data, size_t dataSize);
 int     list_del(List *l, int i);
 int     list_remove(List *l, void *data);
 //int list_pop();
@@ -33,7 +33,7 @@ int     list_remove(List *l, void *data);
 #define list_append(l, data) list_append(l, data, sizeof(data))
 #define list_prepend(l, data) list_prepend(l, data, sizeof(data))
 #define list_insert(l, i, data) list_insert(l, i, data, sizeof(data))
-#define list_put(l, i, data) list_put(l, i, data, sizeof(data))
+#define list_set(l, i, data) list_set(l, i, data, sizeof(data))
 
 #endif /* _LIST_H_ */
 
